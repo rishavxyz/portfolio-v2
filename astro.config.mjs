8fc04e8b24e65f3d38ffd5fd8b4e188e43501f1b
rishavxyz <iamrishav@outlook.in>
2024-02-28 15:26:20 +0530

    update meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-// import compress from "astro-compress";
-import { dev }from "./src/lib/env";
+import compress from "astro-compress";
+
+import { defineConfig } from "astro/config";
+import { dev } from "./src/lib/env";
+
 const site = dev ? "http://localhost:4321" : "https://v2.rishavmandal.xyz";
 
 // https://astro.build/config
 export default defineConfig({
   site,
   prefetch: true,
-  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  integrations: [mdx(), sitemap(), svelte(), tailwind(), compress()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
-      enabled: dev
+      enabled: false,
     },
     isr: {
-      expiration: false
-    }
-  })
+      expiration: false,
+    },
+  }),
 });
